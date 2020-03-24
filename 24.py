4145d2cafeeaa1c829aec229938303d819deb782
ZhangMingYa <myzhang0826@gmail.com>
2020-03-24 16:02:29 +0800

    24.反转一个单链表(迭代)

---
+++ 24.py
@@ -0,0 +1,51 @@
+"""
+反转一个单链表。
+
+示例:
+
+输入: 1->2->3->4->5->NULL
+输出: 5->4->3->2->1->NULL
+进阶:
+你可以迭代或递归地反转链表。你能否用两种方法解决这道题？
+双指针迭代
+我们可以申请两个指针，第一个指针叫 pre，最初是指向 null 的。
+第二个指针 cur 指向 head，然后不断遍历 cur。
+每次迭代到 cur，都将 cur 的 next 指向 pre，然后 pre 和 cur 前进一位。
+都迭代完了(cur 变成 null 了)，pre 就是最后一个节点了。
+
+作者：wang_ni_ma
+链接：https://leetcode-cn.com/problems/fan-zhuan-lian-biao-lcof/solution/dong-hua-yan-shi-duo-chong-jie-fa-206-fan-zhuan-li/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+"""
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        pre = None
+        tmp = None
+        cur = head
+        while cur is not None:
+            # 记录当前节点的下一个节点
+            tmp = cur.next
+            # 然后将当前节点指向pre
+            cur.next = pre
+            # pre和cur节点都前进一位
+            pre = cur
+            cur = tmp
+        return pre
+
+
+if __name__ == '__main__':
+    l = ListNode(1)
+    l.next = ListNode(2)
+    l.next.next = ListNode(3)
+    l.next.next.next = ListNode(4)
+    l.next.next.next.next = ListNode(5)
+    s = Solution()
+    print(s.reverseList(l))
+    print(s.reverseList(l).next)
+    print(s.reverseList(l))
